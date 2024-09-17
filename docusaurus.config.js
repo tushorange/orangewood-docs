fe27f393059db699fa4a3112563c5da8a51130ca
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-09-17 17:56:52 +0530

    fix: issue related to broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,17 +1,15 @@
-// Import necessary modules
 const { themes } = require('prism-react-renderer');
-const fs = require('fs');
 
-// Define code themes
 const code_themes = {
   light: themes.github,
   dark: themes.dracula,
 };
 
-// Define metadata
+/** @type {import('@docusaurus/types').Config} */
 const meta = {
   title: 'Orangewood Labs Docs',
-  tagline: 'Democratizing Robots',
+  tagline:
+    'Democratizing Robots',
   url: 'https://orangewoodwebsite.vercel.app/',
   baseUrl: '/',
   favicon: 'https://res.cloudinary.com/dreradzep/image/upload/v1725252710/fav_rda6mt.png',
@@ -21,12 +19,172 @@ const meta = {
   },
 };
 
-// Define docs settings
+/** @type {import('@docusaurus/plugin-content-docs').Options[]} */
 const docs = [
-  // Define doc sections here (your existing settings)
+    {
+      id: 'cli',
+      path: 'docs/cli',
+      routeBasePath: '/cli',
+    },
+  // {
+  //   id: 'plugin-sdk',
+  //   path: 'docs/plugin-sdk',
+  //   routeBasePath: '/plugin-sdk',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+
+  // Community packages
+  // {
+  //   id: 'community-packages',
+  //   path: 'docs/community-packages',
+  //   routeBasePath: '/community-packages',
+  // },
+
+  // Web UI Kits
+  {
+    id: 'ui-kit',
+    path: 'docs/ui-kit',
+    routeBasePath: '/ui-kit',
+    versions: {
+      current: {
+        label: '1.x.x',
+      },
+    },
+  },
+  {
+    id: 'react-ui-kit',
+    path: 'docs/react-ui-kit',
+    routeBasePath: '/react-ui-kit',
+    versions: {
+      current: {
+        label: '1.x.x',
+      },
+    },
+  },
+  // {
+  //   id: 'angular-ui-kit',
+  //   path: 'docs/angular-ui-kit',
+  //   routeBasePath: '/angular-ui-kit',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+
+  // Web Core
+  {
+    id: 'web-core',
+    path: 'docs/web-core',
+    routeBasePath: '/web-core',
+    versions: {
+      current: {
+        label: '2.x.x',
+      },
+    },
+  },
+  // React Web Core
+  {
+    id: 'react-web-core',
+    path: 'docs/react-web-core',
+    routeBasePath: '/react-web-core',
+    versions: {
+      current: {
+        label: '2.x.x',
+      },
+    },
+  },
+
+  // Mobile Core
+  // {
+  //   id: 'android-core',
+  //   path: 'docs/android-core',
+  //   routeBasePath: '/android-core',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 'flutter-core',
+  //   path: 'docs/flutter-core',
+  //   routeBasePath: '/flutter-core',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 'ios-core',
+  //   path: 'docs/ios-core',
+  //   routeBasePath: '/ios-core',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 'rn-core',
+  //   path: 'docs/rn-core',
+  //   routeBasePath: '/rn-core',
+  //   versions: {
+  //     current: {
+  //       label: '0.x.x',
+  //     },
+  //   },
+  // },
+
+  // Mobile UI Kits
+  // {
+  //   id: 'android',
+  //   path: 'docs/android',
+  //   routeBasePath: '/android',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 'flutter',
+  //   path: 'docs/flutter',
+  //   routeBasePath: '/flutter',
+  //   versions: {
+  //     current: {
+  //       label: '0.7.x',
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 'ios',
+  //   path: 'docs/ios',
+  //   routeBasePath: '/ios',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 'react-native',
+  //   path: 'docs/rn-ui-kit',
+  //   routeBasePath: '/react-native',
+  //   versions: {
+  //     current: {
+  //       label: '1.x.x',
+  //     },
+  //   },
+  // },
 ];
 
-// Define default settings for docs plugin
+/** @type {import('@docusaurus/plugin-content-docs').Options} */
 const defaultSettings = {
   breadcrumbs: true,
   editUrl: 'https://github.com/dyte-io/docs/tree/main/',
@@ -38,7 +196,10 @@ const defaultSettings = {
   sidebarPath: require.resolve('./sidebars-default.js'),
 };
 
-// Function to create a docs plugin
+/**
+ * Create a section
+ * @param {import('@docusaurus/plugin-content-docs').Options} options
+ */
 function create_doc_plugin({
   sidebarPath = require.resolve('./sidebars-default.js'),
   ...options
@@ -54,12 +215,10 @@ function create_doc_plugin({
   ];
 }
 
-// Import plugins
 const { webpackPlugin } = require('./plugins/webpack-plugin.cjs');
 const tailwindPlugin = require('./plugins/tailwind-plugin.cjs');
 const docs_plugins = docs.map((doc) => create_doc_plugin(doc));
 
-// Define plugins
 const plugins = [
   tailwindPlugin,
   ...docs_plugins,
@@ -68,21 +227,228 @@ const plugins = [
     '@docusaurus/plugin-client-redirects',
     {
       createRedirects(path) {
-        // Your existing redirect logic
+        if (path.startsWith('/guides/capabilities/chat/export-chat-dump')) {
+          return ['/capabilities/export-chat-dump'];
+        }
+
+        if (path.startsWith('/guides/capabilities/misc/embed')) {
+          return ['/guides/capabilities/embed'];
+        }
+
+        if (path.startsWith('/react-ui-kit/basics/integrate')) {
+          return ['/react-ui-kit/basics/components-basics'];
+        }
+
+        if (
+          path.startsWith(
+            '/guides/capabilities/misc/livestreaming-other-platforms'
+          )
+        ) {
+          return [
+            '/guides/capabilities/livestreaming-other-platforms',
+            '/guides/capabilities/recording/livestream-recording',
+          ];
+        }
+
+        if (
+          path.startsWith('/guides/capabilities/video/add-virtual-background')
+        ) {
+          return [
+            '/guides/capabilities/middleware/add-virtual-background',
+            '/guides/capabilities/customization/add-virtual-background',
+          ];
+        }
+
+        if (path.startsWith('/guides/live-video/concepts')) {
+          return ['/guides/live-video/concepts-live-video'];
+        }
+
+        if (path.startsWith('/guides/voice-conf/concepts')) {
+          return ['/guides/voice-conf/concepts-voice-conf'];
+        }
+
+        if (path.startsWith('/guides/livestream/concepts')) {
+          return ['/guides/livestream/concepts-ils'];
+        }
+
+        if (path.startsWith(''))
+          if (path.startsWith('/web-core/livestreaming')) {
+            return [
+              path.replace(
+                '/web-core/livestreaming',
+                '/web-core/livestreaming/livestream-apis'
+              ),
+            ];
+          }
+        if (path.startsWith('/rn-core/livestreaming')) {
+          return [
+            path.replace(
+              '/rn-core/livestreaming',
+              '/rn-core/livestreaming/livestream-apis'
+            ),
+          ];
+        }
+        if (path.startsWith('/react-web-core/livestreaming')) {
+          return [
+            path.replace(
+              '/react-web-core/livestreaming',
+              '/react-web-core/livestreaming/livestream-apis'
+            ),
+          ];
+        }
+        if (path.startsWith('/web-core/stage')) {
+          return [
+            path.replace(
+              '/web-core/stage',
+              '/web-core/livestreaming/state-management-apis'
+            ),
+          ];
+        }
+        if (path.startsWith('/rn-core/stage')) {
+          return [
+            path.replace(
+              '/rn-core/stage',
+              '/rn-core/livestreaming/state-management-apis'
+            ),
+          ];
+        }
+        if (path.startsWith('/react-web-core/stage')) {
+          return [
+            path.replace(
+              '/react-web-core/stage',
+              '/react-web-core/livestreaming/state-management-apis'
+            ),
+          ];
+        }
+        if (path.startsWith('/guides/capabilities/webhooks')) {
+          return [
+            path.replace('/guides/capabilities/webhooks', '/guides/webhooks'),
+            path.replace(
+              '/guides/capabilities/webhooks',
+              '/guides/features/webhooks'
+            ),
+          ];
+        }
+        if (path.startsWith('/guides/capabilities/recording')) {
+          return [
+            path.replace('/guides/capabilities/recording', '/guides/recording'),
+            path.replace(
+              '/guides/capabilities/recording',
+              '/guides/features/recording'
+            ),
+          ];
+        }
+        if (path.startsWith('/guides/capabilities/recording')) {
+          return [
+            path.replace('/guides/capabilities/recording', '/guides/recording'),
+            path.replace(
+              '/guides/capabilities/recording',
+              '/guides/features/recording'
+            ),
+          ];
+        }
+        if (path.startsWith('/guides/capabilities/embed')) {
+          return [
+            path.replace('/guides/capabilities/embed', '/guides/embed'),
+            path.replace(
+              '/guides/capabilities/embed',
+              '/guides/features/embed'
+            ),
+          ];
+        }
+        if (path.startsWith('/guides/capabilities/export-chat-dump')) {
+          return [
+            path.replace(
+              '/guides/capabilities/export-chat-dump',
+              '/guides/export-chat-dump'
+            ),
+            path.replace(
+              '/guides/capabilities/export-chat-dump',
+              '/guides/features/export-chat-dump'
+            ),
+          ];
+        }
+        if (path.startsWith('/guides/capabilities/breakoutroom')) {
+          return [
+            path.replace(
+              '/guides/capabilities/breakoutroom',
+              '/guides/breakoutroom'
+            ),
+            path.replace(
+              '/guides/capabilities/breakoutroom',
+              '/guides/features/breakoutroom'
+            ),
+          ];
+        }
+        /* for everything else */
+        if (path.startsWith('/guides/capabilities')) {
+          return [path.replace('/guides/capabilities', '/guides/features')];
+        }
+        if (path === '/ui-kit') {
+          return [
+            '/javascript/advanced-usage',
+            '/javascript/customize-meeting-ui',
+            '/javascript/events',
+            '/javascript/installation',
+            '/javascript/quickstart',
+            '/javascript/reference/chat-message',
+            '/javascript/reference/connection-config',
+            '/javascript/reference/dyte-client',
+            '/javascript/reference/dyte-control-bar',
+            '/javascript/reference/dyte-errors',
+            '/javascript/reference/dyte-grid',
+            '/javascript/reference/dyte-meeting-events',
+            '/javascript/reference/dyte-plugin',
+            '/javascript/reference/dyte-ui-config',
+            '/javascript/reference/meeting',
+            '/javascript/reference/participant',
+            '/javascript/reference/self-participant',
+            '/javascript/sample-app',
+            '/javascript/usage',
+            '/javascript/virtual-background',
+            '/javascript/',
+          ];
+        }
+        if (path === '/react-ui-kit') {
+          return [
+            '/react/advanced-usage',
+            '/react/customize-meeting-ui',
+            '/react/events',
+            '/react/installation',
+            '/react/quickstart',
+            '/react/reference/chat-message',
+            '/react/reference/connection-config',
+            '/react/reference/dyte-client',
+            '/react/reference/dyte-control-bar',
+            '/react/reference/dyte-errors',
+            '/react/reference/dyte-grid',
+            '/react/reference/dyte-meeting-events',
+            '/react/reference/dyte-plugin',
+            '/react/reference/dyte-ui-config',
+            '/react/reference/meeting',
+            '/react/reference/participant',
+            '/react/reference/self-participant',
+            '/react/sample-app',
+            '/react/usage',
+            '/react/virtual-background',
+            '/react/',
+          ];
+        }
+        return undefined; // Return a falsy value: no redirect created
       },
     },
   ],
 ];
 
-// Read HTML files
+const fs = require('fs');
 const sdksHTML = fs.readFileSync('./src/snippets/sdks.html', 'utf-8');
 const resourcesHTML = fs.readFileSync('./src/snippets/resources.html', 'utf-8');
 
-// Define main configuration
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   ...meta,
   plugins,
+
   trailingSlash: false,
   themes: ['@docusaurus/theme-live-codeblock', '@docusaurus/theme-mermaid'],
   clientModules: [require.resolve('./src/client/define-ui-kit.js')],
@@ -117,61 +483,171 @@ const config = {
       }),
     ],
   ],
-  themeConfig: {
-    image: '/img/dyte-docs-card.png',
-    colorMode: {
-      defaultMode: 'light',
-    },
-    docs: {
-      sidebar: {
-        autoCollapseCategories: true,
-        hideable: true,
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: '/img/dyte-docs-card.png',
+      colorMode: {
+        defaultMode: 'light',
       },
-    },
-    navbar: {
-      logo: {
-        href: '/',
-        src: '/logo/ow-black.png',
-        srcDark: '/logo/OWL-Logo-white.png',
-        alt: 'Orangewood Documentation | Ornagewood Docs',
-        height: '50px',
-        width: 'auto',
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true,
+          hideable: true,
+        },
       },
-      items: [
-        // Your existing navbar items
-      ],
-    },
-    footer: {
-      logo: {
-        href: '/',
-        src: 'https://res.cloudinary.com/dreradzep/image/upload/v1723783987/logo-white_kw3yma.png',
-        srcDark: 'https://res.cloudinary.com/dreradzep/image/upload/v1723783987/logo-white_kw3yma.png',
-        alt: 'Orangewood Documentation | Ornagewood Docs',
-        height: '36px',
+      navbar: {
+        logo: {
+          href: '/',
+          src: '/logo/ow-black.png',
+          srcDark: '/logo/OWL-Logo-white.png',
+          alt: 'Orangewood Documentation | Ornagewood Docs',
+          height: '50px',
+          width: 'auto',
+        },
+        items: [
+          {
+            label: 'Docs',
+            to: 'guides',
+            className: 'guides-top-header',
+          },
+          {
+            label: 'Dummy Text 1',
+            type: 'dropdown',
+            className: 'dyte-dropdown',
+            items: [
+              {
+                type: 'html',
+                value: sdksHTML,
+                className: 'dyte-dropdown',
+              },
+            ],
+          },
+          {
+            label: 'RobotGPT',
+            to: 'https://orangewoodwebsite.vercel.app/robo-gpt',
+          },
+          {
+            label: 'Help',
+            type: 'dropdown',
+            className: 'orangewood-dropdown resources-dropdown',
+            items: [
+              {
+                type: 'html',
+                value: resourcesHTML,
+                className: 'orangewood-dropdown',
+              },
+            ],
+          },
+          {
+            label: 'Support',
+            to: 'https://orangewoodwebsite.vercel.app/blogs',
+          },
+
+          // {
+          //   type: 'search',
+          //   position: 'right',
+          // },
+          {
+            label: 'Book a demo',
+            href: 'https://calendly.com/pratik-b-orangewood/30min',
+            position: 'right',
+            className: 'navbar-book-demo',
+          },
+          // {
+          //   label: 'Sign Up',
+          //   href: 'https://dev.dyte.io/register',
+          //   position: 'right',
+          //   className: 'dev-portal-signup dev-portal-link',
+          // },
+        ],
       },
-      links: [
-        // Your existing footer links
-      ],
-      copyright: 'Copyright © orangewood since 2017. All rights reserved.',
-    },
-    prism: {
-      theme: code_themes.light,
-      darkTheme: code_themes.dark,
-      additionalLanguages: [
-        // Your existing additional languages
-      ],
-      magicComments: [
-        // Your existing magic comments
-      ],
-    },
-    algolia: {
-      appId: 'HL0HSV62RK',
-      apiKey: '72ebf02146698733b7114c7b36da0945',
-      indexName: 'docs',
-      contextualSearch: true,
-      searchParameters: {},
-    },
-  },
+      footer: {
+        logo: {
+          href: '/',
+          src: 'https://res.cloudinary.com/dreradzep/image/upload/v1723783987/logo-white_kw3yma.png',
+          srcDark: 'https://res.cloudinary.com/dreradzep/image/upload/v1723783987/logo-white_kw3yma.png',
+          alt: 'Orangewood Documentation | Ornagewood Docs',
+          height: '36px',
+        },
+        links: [
+          {
+            title: 'Company',
+            items: [
+              {
+                label: 'About Us',
+                href: 'https://www.orangewood.co/',
+              },
+              {
+                label: 'Join Us',
+                href: 'https://www.orangewood.co/',
+              },
+              {
+                label: 'Privacy Policy',
+                href: 'https://www.orangewood.co/',
+              },
+              {
+                label: 'Contact Us',
+                href: 'https://www.orangewood.co/',
+              },
+            ],
+          },
+          {
+            title: 'Resources',
+            items: [
+              {
+                label: 'Documentation',
+                href: '/',
+              },
+              {
+                label: 'Blog',
+                href: 'https://www.orangewood.co/',
+              },
+              {
+                label: 'Community',
+                href: 'https://www.orangewood.co/',
+              },
+            ],
+          },
+        ],
+        copyright: 'Copyright © orangewood since 2017. All rights reserved.',
+      },
+      prism: {
+        theme: code_themes.light,
+        darkTheme: code_themes.dark,
+        additionalLanguages: [
+          'dart',
+          'ruby',
+          'groovy',
+          'kotlin',
+          'java',
+          'swift',
+          'objectivec',
+          'json',
+          'bash',
+        ],
+        magicComments: [
+          {
+            className: 'theme-code-block-highlighted-line',
+            line: 'highlight-next-line',
+            block: { start: 'highlight-start', end: 'highlight-end' },
+          },
+          {
+            className: 'code-block-error-line',
+            line: 'highlight-next-line-error',
+          },
+        ],
+      },
+      algolia: {
+        appId: 'HL0HSV62RK',
+        apiKey: '72ebf02146698733b7114c7b36da0945',
+        indexName: 'docs',
+        contextualSearch: true,
+        searchParameters: {},
+      },
+    }),
+
   webpack: {
     jsLoader: (isServer) => ({
       loader: require.resolve('swc-loader'),
@@ -189,8 +665,8 @@ const config = {
       },
     }),
   },
-  // Add the onBrokenLinks option here
-  onBrokenLinks: 'ignore',
+
+  onBrokenLinks: 'ignore',  // Add this line at the root level
 };
 
 module.exports = config;
